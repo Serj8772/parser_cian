ca17f5275c0368f154d0efa5902e068aeec92e50
Serj8772 <icq66623702@ya.ru>
2024-12-12 14:50:09 +0300

    fix bugs

--- settings.py
+++ settings.py
@@ -7,7 +7,7 @@ id_channel = ''   # телеграм канал
 id_channel_for_logs = ''   # телеграм канал
 
 # ссылка на поиск
-link=''
+link='https://lobnya.cian.ru/cat.php?deal_type=rent&engine_version=2&offer_type=flat&region=4848&room1=1&room2=1&sort=creation_date_desc&type=4'
 
 # проверка на анонимность
 # link='https://intoli.com/blog/not-possible-to-block-chrome-headless/chrome-headless-test.html' # проверка на анонимность
